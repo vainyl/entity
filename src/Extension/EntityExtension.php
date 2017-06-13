b5a37419c2c610b30a05a3a64f3a2c4e25f9726a
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-13 12:53:15 +0300

    Added EntityExtension

---
+++ src/Extension/EntityExtension.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Entity
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Entity\Extension;
+
+use Vainyl\Core\Extension\AbstractExtension;
+
+/**
+ * Class EntityExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class EntityExtension extends AbstractExtension
+{
+}
