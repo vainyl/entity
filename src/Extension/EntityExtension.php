59d6be51fa2ed39590f52a47fd182188f8a20023
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-14 14:36:23 +0300

    Added EntityExtension

--- src/Extension/EntityExtension.php
+++ src/Extension/EntityExtension.php
@@ -12,6 +12,8 @@ declare(strict_types=1);
 
 namespace Vainyl\Entity\Extension;
 
+use Symfony\Component\DependencyInjection\Alias;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Vainyl\Core\Extension\AbstractExtension;
 
 /**
@@ -21,4 +23,22 @@ use Vainyl\Core\Extension\AbstractExtension;
  */
 class EntityExtension extends AbstractExtension
 {
+    /**
+     * @inheritDoc
+     */
+    public function load(array $configs, ContainerBuilder $container): AbstractExtension
+    {
+        $configuration = new EntityConfiguration();
+        $documentConfiguration = $this->processConfiguration($configuration, $configs);
+
+        $container->removeDefinition('database.entity');
+        $container->removeDefinition('entity.operation.factory');
+        $container->setAlias('database.entity', new Alias('database.entity.' . $documentConfiguration['orm']));
+        $container->setAlias(
+            'entity.operation.factory',
+            new Alias('entity.operation.factory' . $documentConfiguration['orm'])
+        );
+
+        return parent::load($configs, $container);
+    }
 }
