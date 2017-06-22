c33375c6d925e479f2b400ea8e2ff456e03c06cf
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-22 17:56:56 +0300

    Added entityfactory exception

--- src/Factory/EntityFactoryInterface.php
+++ src/Factory/EntityFactoryInterface.php
@@ -12,21 +12,13 @@ declare(strict_types=1);
 
 namespace Vainyl\Entity\Factory;
 
-use Vainyl\Core\IdentifiableInterface;
-use Vainyl\Entity\EntityInterface;
+use Vainyl\Core\ArrayX\Factory\ArrayFactoryInterface;
 
 /**
  * Interface EntityFactoryInterface
  *
  * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
  */
-interface EntityFactoryInterface extends IdentifiableInterface
+interface EntityFactoryInterface extends ArrayFactoryInterface
 {
-    /**
-     * @param string $name
-     * @param array  $entityData
-     *
-     * @return EntityInterface
-     */
-    public function create(string $name, array $entityData = []) : EntityInterface;
 }
