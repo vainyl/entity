08d602ce54a0b4c7424ebe6b0e2e13e5bee4abc2
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-08-03 15:53:33 +0300

    Refactored registry

--- src/Extension/EntityExtension.php
+++ src/Extension/EntityExtension.php
@@ -55,13 +55,13 @@ class EntityExtension extends AbstractFrameworkExtension
         $factoryDefinition = $container->findDefinition('entity.operation.factory');
         $factoryDefinition->replaceArgument(
             0,
-            sprintf('entity.operation.factory.%', $entityConfiguration['factory'])
+            sprintf('entity.operation.factory.%s', $entityConfiguration['factory'])
         );
         if (false === $container->hasDefinition('entity.hydrator')) {
             throw new MissingRequiredServiceException($container, 'entity.hydrator');
         }
         $hydratorDefinition = $container->findDefinition('entity.hydrator');
-        $hydratorDefinition->replaceArgument(0, sprintf('entity.hydrator.%', $entityConfiguration['factory']));
+        $hydratorDefinition->replaceArgument(0, sprintf('entity.hydrator.%s', $entityConfiguration['factory']));
         if (false === $container->hasDefinition('database.entity')) {
             throw new MissingRequiredServiceException($container, 'database.entity');
         }
